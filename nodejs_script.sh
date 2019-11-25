401a60eb230f02bb9bdb5416e248be834149faec
Daniel Arvai <danielarvai@gmail.com>
2019-11-25 12:10:37 +0100

    Another nodejs binary install

--- nodejs_script.sh
+++ nodejs_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 
 cd /var/lib/jenkins
-wget  -P /var/lib/jenkins https://nodejs.org/download/
-
-mkdir -p /var/lib/jenkins/nodejs
-tar -zxvf node-v8.4.0-linux-x64.tar.gz -C /var/lib/jenkins/nodejs
-export PATH=$PATH:/var/lib/jenkins/nodejs/node-v8.4.0-linux-x64/bin
+wget http://nodejs.org/dist/v0.10.30/node-v0.10.30.tar.gz
+tar xzvf node-v* && cd node-v*
+sudo yum install gcc gcc-c++
+./configure
+make
+sudo make install
+node --version
