3eaf5ca974fc067f38d47390083528a14a23b44c
Daniel Arvai <danielarvai@gmail.com>
2019-11-25 09:47:43 +0100

    starting firewall, open jenkins port for firewall and start jenkins

--- jenkins_installation.sh
+++ jenkins_installation.sh
@@ -3,3 +3,15 @@
 yum install -y java-1.8.0-openjdk-devel
 curl http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+yum install -y git
+
+systemctl start firewalld
+systemctl enable firewalld
+
+firewall-cmd --add-port=8080/tcp --permanent
+firewall-cmd --reload
+
+yum install -y jenkins
+systemctl start jenkins
+systemctl enable jenkins
