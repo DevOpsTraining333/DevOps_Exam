454a1a97baab1573c72be4bac6453533acfc7ac5
Daniel Arvai <danielarvai@gmail.com>
2019-11-25 11:53:21 +0100

    different nodejs installation in nodejs_script.sh

--- nodejs_script.sh
+++ nodejs_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 sudo yum install -y gcc-c++ make nc -y
-sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-sleep 2
+sudo yum install epel-release
 sudo yum install nodejs
+sudo yum install npm
 
 sudo npm install supervisor -g
 sudo supervisor index.js &
