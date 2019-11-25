cb992d3345d83c8ca161fdadc8a0133c81d0a566
Daniel Arvai <danielarvai@gmail.com>
2019-11-25 10:14:57 +0100

    Adding index file for nodejs

---
+++ index.js
@@ -0,0 +1,5 @@
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('DevOps_exam \n');
+  }).listen(6543, '192.168.56.112');
+  console.log('Server running at http://192.168.56.112:6543/');
